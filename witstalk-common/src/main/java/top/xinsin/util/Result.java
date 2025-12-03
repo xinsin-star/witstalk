9599a7e8230759a5f5af2aa41a0b54ae4d68288d
wzp <minecraftwzpmc@163.com>
2025-12-03 21:48:21 +0800

    feat: add some result utils function

--- witstalk-common/src/main/java/top/xinsin/util/Result.java
+++ witstalk-common/src/main/java/top/xinsin/util/Result.java
@@ -1,8 +1,19 @@
 package top.xinsin.util;
 
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 @Data
+@Slf4j
 public class Result<T> {
     private Integer code = 200;
     private String msg = "success";
@@ -13,36 +24,53 @@ public class Result<T> {
     private Result() {}
 
     public static  <T> Result<T> success() {
-        return new Result<T>();
+        return new Result<>();
     }
     public static  <T> Result<T> success(T data) {
         Result<T> result = new Result<>();
         result.setData(data);
         return result;
     }
-    public static  <T> Result<T> success(String msg, T data) {
+    public static  <T> Result<T> success(@NonNull String msg, T data) {
         Result<T> result = new Result<>();
         result.setMsg(msg);
         result.setData(data);
         return result;
     }
-    public static <T> Result<T> fail(String msg) {
+    public static <T> Result<T> fail(@NonNull String msg) {
         Result<T> result = new Result<>();
         result.setCode(500);
         result.setMsg(msg);
         return result;
     }
-    public static <T> Result<T> fail(Integer code, String msg) {
+    public static <T> Result<T> fail(@NonNull Integer code, @NonNull String msg) {
         Result<T> result = new Result<>();
         result.setCode(code);
         result.setMsg(msg);
         return result;
     }
-    public static <T> Result<T> fail(Integer code, String msg, String error) {
+    public static <T> Result<T> fail(@NonNull Integer code, @NonNull String msg, @NonNull String error) {
         Result<T> result = new Result<>();
         result.setCode(code);
         result.setMsg(msg);
         result.setError(error);
         return result;
+    }
+
+    public static <T> Result<T> fail(@NonNull HttpStatus code, @NonNull String msg) {
+        return fail(code.value(), msg);
+    }
+
+    public void writeToResponse(HttpServletResponse response) {
+        response.addHeader("Content-Type", "application/json; charset=utf-8");
+        try (ServletOutputStream outputStream = response.getOutputStream()) {
+            writeToOutputStream(outputStream);
+        } catch (IOException e) {
+            log.trace("写出到流失败，", e);
+        }
+    }
+
+    public void writeToOutputStream(OutputStream stream) throws IOException {
+        stream.write(JSON.toJSONString(this).getBytes(StandardCharsets.UTF_8));
     }
 }
